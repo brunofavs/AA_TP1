55fd2a57d6a39d2acd621c7509a0ad2677400ab2
brunofavs <bruno.favs@ua.pt>
2023-11-05 20:44:36 +0000

    Brute force implemenation working

--- src/lib/dominatingSet.py
+++ src/lib/dominatingSet.py
@@ -7,33 +7,50 @@ import matplotlib.pyplot as plt
 import networkx as nx
 from tabulate import tabulate
 
-def computeDominatingSet(G,subsets):
+def computeDominatingSet(G,subset):
     """
-    This function should take in a graph and its subsets and exclude the non-dominant sets and return a list of
-    dominant sets
+    This function should take in a graph and its subset and determine if it is a dominant set
     """
 
     assert isinstance(
         G, nx.Graph
     ), f"Input should be a networkX undirected graph object ({type(nx.Graph())}\n Input type was {type(G)}"
 
-    dominating_sets = []
+    print(f'\n\nAnalyzing if subset {subset} is dominant')
 
-    for subset in subsets:
+    outsider_nodes = [node for node in G.nodes() if node not in subset]
+
+    for outsider_node in outsider_nodes:
+        outsider_neighbours = list(G.neighbors(outsider_node))
+
+        print(f'Neighbours of outsider node {outsider_node} are {outsider_neighbours}')
+    
+        '''
+        Loops through every node in subset
+        Checks if it is in outsider neighbours
+        Adds either True or False to the list
+        If the list has atleast 1 True intersection returns True otherwise returns False
+        '''
+
+        # * Could use either approach
+        intersection = any(node in outsider_neighbours for node in subset)
+        intersection_list = [node for node in outsider_neighbours if node in subset]
+
+        print(f"Outsider node neighbours {intersection_list} are also subset's nodes")
+
+        # If there isn't a single outsider node neighbour also a subset node then its not dominant
+        if not intersection:
+            print(f'Neither neighbour of the outsider node {outsider_node} is in subset {subset}, this is NOT a dominating set')
+            return False
+
+    print(f'The subset {subset} is dominant')
+    return True
         
-        outsider_nodes = [node for node in G.nodes() if node not in subset]
-
-        for outsider_node in outsider_nodes:
-            # outsider
-            pass
-        # TODO Get outsider neighbours, check if they are in subset, if not its not a subset
-            
 
 
 
 
 
-    return dominating_sets
 
 
 
--- src/main.py
+++ src/main.py
@@ -12,49 +12,99 @@ from lib.randomGraphGenerators import generateRandomUndirectedGraph
 from lib.generateGraphSubsets import generateGraphSubsets
 from lib.dominatingSet import computeDominatingSet
 
+
 # Lowest weight dominating set
-class LWDS():
-    def __init__(self,n_nodes,density):
+class LWDS:
+    def __init__(self, n_nodes, density):
         self.n_nodes = n_nodes
         self.ideal_density = density
-        self.graph = generateRandomUndirectedGraph(n_nodes,density)
+        self.graph = generateRandomUndirectedGraph(n_nodes, density)
+        self.dominant_sets = []
+        self.non_dominant_sets = []
+        self.lightest_dominating_sets = []
 
-        print(f'Generating a random undirected graph with {n_nodes} nodes and {density*100}% density') 
-        print(f'Actual density of graph is {nx.density(self.graph)*100}%')
-    
-    def computeSubsets(self):
+        print(
+            f"Generating a random undirected graph with {n_nodes} nodes and {density*100}% density"
+        )
+        print(f"Actual density of graph is {nx.density(self.graph)*100}%")
+
+    def computeAllSubsets(self):
         # ! For big graphs this crashes the PC
         self.graph_subsets = generateGraphSubsets(self.graph)
-    
+
     def computeDominantSets(self):
-        computeDominatingSet(self.graph,self.graph_subsets)
+        for subset in self.graph_subsets:
+            is_dominant = computeDominatingSet(self.graph, subset)
+
+            if is_dominant:
+                self.dominant_sets.append(subset)
+            else:
+                self.non_dominant_sets.append(subset)
+
+        print(f"All dominant sets are {self.dominant_sets}")
+        print(f"\n\nAll NON dominant sets are {self.non_dominant_sets}")
+
+    def findLightestSets(self, subsets):
+        all_weights = nx.get_node_attributes(self.graph, "weight")
+        subsets_weights = np.array([])
+
+        for subset in subsets:
+            subset_weight = sum([all_weights[node] for node in subset])
+            print(f"Subset's {subset} weigth is {subset_weight}")
+
+            subsets_weights = np.append(subsets_weights, subset_weight)
+
+        # This should return all possible minimum sets, and not just one
+        minimum_idxs = np.where(subsets_weights == subsets_weights.min())
+
+        # The [0] is necessary because np.where outputs a tuple of arrays
+        print(f"The lightest dominant sets are : ")
+        for idx in minimum_idxs[0]:
+            self.lightest_dominating_sets.append(subsets[idx])
+            print(f"    -> {subsets[idx]},weigth = {subsets_weights[idx]}")
+
+    def bruteForceDominantSets(self):
+        self.computeAllSubsets()
+        self.computeDominantSets()
+        self.findLightestSets(self.dominant_sets)
 
     def draw(self):
+        # * Get the positions from the node's attributes
+        pos = nx.get_node_attributes(self.graph, "pos")
+        weights = nx.get_node_attributes(self.graph, "weight")
 
-        #* Get the positions from the node's attributes
-        pos = nx.get_node_attributes(self.graph, 'pos')
-        weights = nx.get_node_attributes(self.graph,'weight')
+        labels = {
+            node: f"""#{node}
+w: {self.graph.nodes[node]['weight']}
+{self.graph.nodes[node]['pos']}"""
+            for node in self.graph.nodes
+        }
 
-        #* Draw the graph with coordinates
-        nx.draw(self.graph, pos,labels = weights, with_labels=True, node_color='skyblue')
-        # nx.draw(self.graph, pos, with_labels=True, node_color='skyblue')
+        # * Draw the graph with coordinates
+        nx.draw(
+            self.graph,
+            pos,
+            labels=labels,
+            with_labels=True,
+            node_color="skyblue",
+            font_size=12,
+            node_size=2500,
+        )
         plt.show()
 
+
 def main():
-    config = {"rng_seed" : 98374}
+    config = {"rng_seed": 98374}
     random.seed(config["rng_seed"])
     np.random.seed(config["rng_seed"])
 
-    n_nodes = 15
-    density = 0.5
+    n_nodes = 30
+    density = 1
 
-    G = LWDS(n_nodes,density)
-    G.computeSubsets()
-    # G.computeDominantSets()
+    G = LWDS(n_nodes, density)
+    G.bruteForceDominantSets()
     G.draw()
 
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
